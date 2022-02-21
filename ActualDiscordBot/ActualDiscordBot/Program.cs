64a435d2ba5752d8af19279cc90738990a179d52
Morangutan <kylianfrags@gmail.com>
2022-02-21 23:08:56 +0100

    discord bot no commands

---
+++ ActualDiscordBot/ActualDiscordBot/Program.cs
@@ -0,0 +1,15 @@
+﻿using ActualDiscordBot;
+using System;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new();
+            bot.RunAsync().GetAwaiter().GetResult();
+        }
+    }
+
+}
